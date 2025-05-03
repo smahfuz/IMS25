6681330c9419cf0f22556ab28822b1d9efd737b2
smahfuz <mahfuz.duetcse@gmail.com>
2025-05-03 21:03:30 +0600

    warehouse controller add

---
+++ IMS.Web/Controllers/WarehouseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Web.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
